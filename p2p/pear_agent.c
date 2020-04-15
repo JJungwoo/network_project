e6b37950947e15e8b86b2d8beca2270efd559f1d
JJungwoo <jungwoo5759@gmail.com>
2020-04-16 02:06:37 +0900

    p2p program Makefile, pear_agent.c code update

--- p2p/pear_agent.c
+++ p2p/pear_agent.c
@@ -1,5 +1,7 @@
 #include "pear_agent.h"
 
+pconn gpconn;
+
 void print_info()
 {
 	printf("=============================\n");
@@ -9,10 +11,14 @@ void print_info()
 
 int create_socket()
 {
-	
+	if((gpconn.target_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	{
+		printf("socket failed \n");
+		return -1;	
+	}
+	return 0;
 }
 
-pconn gpconn;
 
 int main(int argc, char *argv[])
 {
@@ -23,7 +29,8 @@ int main(int argc, char *argv[])
 	}
 	print_info();
 
-
+	create_socket();
+	printf("%d\n", gpconn.target_sockfd);
 
 
 	return 0;
