4f820ae55d618407762e10dbee63e2b992117e5a
jungwoo <jungwoo5759@gmail.com>
2020-04-16 14:24:04 +0900

    Update p2p program server code & Makefile - pear_agent에 서버 측 소켓 통신 코드 구현 - Makefile pear_agent 파일로 타겟 설정 및 내용 정리

--- p2p/pear_agent.c
+++ p2p/pear_agent.c
@@ -5,33 +5,110 @@ pconn gpconn;
 void print_info()
 {
 	printf("=============================\n");
-	printf("= pear to pear agent \n");
+	printf("#    pear to pear agent     #\n");
 	printf("=============================\n");
 }
 
 int create_socket()
 {
-	if((gpconn.target_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	int ret = 0;
+
+	ret = gpconn.recv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(ret == FAIL) 
 	{
-		printf("socket failed \n");
-		return -1;	
+		printf("[create_socket] socket failed (ret:%d) \n", ret);
+		return FAIL;
 	}
-	return 0;
+
+	return ret;
 }
 
-
-int main(int argc, char *argv[])
+int create_conn_st(int port)
 {
-	
-	if(argc <= 1){
-		printf("bad usage\n");
-		return 0;
+	int ret = 0;
+
+	gpconn.recvaddr.sin_family = AF_INET;
+	gpconn.recvaddr.sin_port = htons(port);
+	gpconn.recvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	ret = bind(gpconn.recv_sockfd, (struct sockaddr*)&gpconn.recvaddr, sizeof(gpconn.recvaddr));
+	if(ret == FAIL)
+	{
+		printf("[create_conn_st] bind failed (ret:%d) \n", ret);
+		return FAIL;
 	}
-	print_info();
 
-	create_socket();
-	printf("%d\n", gpconn.target_sockfd);
+	ret = listen(gpconn.recv_sockfd, 5);
+	if(ret == FAIL)
+	{
+		printf("[create_conn_st] listen failed (ret:%d) \n", ret);
+		return FAIL;
+	}
 
+	return ret;
+}
+
+void close_socket()
+{
+	close(gpconn.recv_sockfd);
+}
+
+int main(int argc, char **argv)
+{
+	int ret = 0;
+	if (argc-1 <= 0)
+	{
+		printf("bad usage\n");
+		print_info();
+		return OK;
+	}
+
+	for(int i=0;i<argc;i++)
+	{
+		if(*argv[i] == '-')
+		{
+			switch(*(argv[i]+1))
+			{
+				case 'h':
+				print_info();
+				return OK;
+				break;
+			}
+		}
+		
+	}
+
+	ret = create_socket();
+	if(ret == FAIL) 
+	{
+		printf("create socker failed (ret:%d) \n", ret);
+		return FAIL;
+	}
+
+	ret = create_conn_st(9999);
+	if(ret == FAIL) 
+	{
+		printf("create_conn_st failed (ret:%d) \n", ret);
+		return FAIL;
+	}
+
+	printf("%d\n", gpconn.recv_sockfd);
+
+	while(1)
+	{
+		printf("Listening...\n");
+		gpconn.send_sockfd = accept(gpconn.recv_sockfd, (struct sockaddr*)&gpconn.recvaddr, (socklen_t*)sizeof(gpconn.sendaddr));
+
+		if(gpconn.send_sockfd == FAIL)
+		{
+			printf("accept failed (ret:%d) \n", gpconn.send_sockfd);
+			return FAIL;
+		}
+
+		printf("Connected Client \n");
+	}
+
+	close_socket();
 
 	return 0;
 }
--- p2p/pear_agent.h
+++ p2p/pear_agent.h
@@ -8,10 +8,20 @@
 
 #define BUF_LEN 1024
 
+// function failed
+#define FAIL -1
+#define OK 0
+
 typedef struct pear_conn_st{
-	int target_sockfd;
-	int target_len;
-	struct sockaddr_in targetaddr;
+	// server 
+	int recv_sockfd;
+	int recv_len;
+	struct sockaddr_in recvaddr;
+
+	// client 
+	int send_sockfd;
+	struct sockaddr_in sendaddr;
+
 	char buffer[BUF_LEN];
 }pconn;
 
